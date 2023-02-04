68dd572fcbbbaf2445aadacee30166d50971c762
tiwaria <tiwaria@opentext.com>
2023-02-04 15:54:42 -0500

    [Graph -> Union Find] : Max Stones From Quarry

--- src/main/java/prep/ds/algo/graphs/union/find/RemoveMaxStonesFromQuarry.java
+++ src/main/java/prep/ds/algo/graphs/union/find/RemoveMaxStonesFromQuarry.java
@@ -1,2 +1,96 @@
-package prep.ds.algo.graphs.union.find;public class RemoveMaxStonesFromQuarry {
+package prep.ds.algo.graphs.union.find;
+
+import java.util.*;
+/**
+ * On a 2D plane, we place n stones at some integer coordinate points. Each coordinate point may have at most one stone.
+ *
+ * A stone can be removed if it shares either the same row or the same column as another stone that has not been removed.
+ *
+ * Given an array stones of length n where stones[i] = [xi, yi] represents the location of the ith stone, return the largest possible number of stones that can be removed.
+ */
+public class RemoveMaxStonesFromQuarry {
+
+    static class UnionFind {
+        private int[] parent;
+        private int[] rank;
+
+        public UnionFind(int nodes) {
+            this.parent = new int[nodes];
+            this.rank = new int[nodes];
+            for (int i = 0; i < nodes; i++) {
+                parent[i] = i;
+                rank[i] = 1;
+            }
+        }
+
+        public int find(int node) {
+            if (parent[node] != node) {
+                return parent[node] = find(parent[node]);
+            }
+            return node;
+        }
+
+        public void union(int x, int y) {
+            int xParent = find(x);
+            int yParent = find(y);
+
+            if (xParent == yParent) {
+                return;
+            }
+
+            if (rank[xParent] > rank[yParent]) {
+                parent[yParent] = xParent;
+                rank[xParent] += rank[yParent];
+            } else if (rank[xParent] < rank[yParent]) {
+                parent[xParent] = yParent;
+                rank[yParent] += rank[xParent];
+            } else {
+                parent[yParent] = xParent;
+                rank[xParent]++;
+            }
+        }
+
+        public int findUniqueGroupsCount() {
+            Set<Integer> roots = new HashSet<>();
+
+            for (int node : parent) {
+                roots.add(node);
+            }
+
+            return roots.size();
+        }
+    }
+
+    public int removeStones(int[][] stones) {
+        int numberOfStones = stones.length;
+        int totalGroups = 0;
+        Map<Integer, List<Integer>> adjacencyMap = new HashMap<>();
+        for (int i = 0; i < stones.length; i++) {
+            for (int j = i + 1; j < stones.length; j++) {
+                if (isSameRowOrColumn(stones[i], stones[j])) {
+                    adjacencyMap.computeIfAbsent(i, v -> new ArrayList<>()).add(j);
+                    adjacencyMap.computeIfAbsent(j, v -> new ArrayList<>()).add(i);
+                }
+            }
+        }
+
+        UnionFind unionFind = new UnionFind(numberOfStones);
+
+        for (int i = 0; i < numberOfStones; i++) {
+            if (adjacencyMap.containsKey(i)) {
+                for (int neighbor : adjacencyMap.get(i)) {
+                        unionFind.union(i, neighbor);
+                }
+            }
+        }
+
+        totalGroups = unionFind.findUniqueGroupsCount();
+
+        return numberOfStones - totalGroups;
+    }
+
+    boolean isSameRowOrColumn(int[] stoneA, int[] stoneB) {
+        return (stoneA[0] == stoneB[0]) || (stoneA[1] == stoneB[1]);
+    }
+
 }
--- src/test/java/prep/ds/algo/graphs/union/find/RemoveMaxStonesFromQuarryTest.java
+++ src/test/java/prep/ds/algo/graphs/union/find/RemoveMaxStonesFromQuarryTest.java
@@ -1,4 +1,42 @@
-import static org.junit.jupiter.api.Assertions.*;
+package prep.ds.algo.graphs.union.find;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 class RemoveMaxStonesFromQuarryTest {
-  
+
+    private RemoveMaxStonesFromQuarry removeMaxStonesFromQuarry;
+
+    @BeforeEach
+    public void setup() {
+        removeMaxStonesFromQuarry = new RemoveMaxStonesFromQuarry();
+    }
+
+    @Test
+    void testRemoveStones_1() {
+        int[][] stones = {{0, 0}, {0, 1}, {1, 0}, {1, 2}, {2, 1}, {2, 2}};
+
+        int maxStones = removeMaxStonesFromQuarry.removeStones(stones);
+
+        Assertions.assertEquals(5, maxStones);
+    }
+
+    @Test
+    void testRemoveStones_2() {
+        int[][] stones = {{0, 0}, {0, 2}, {1, 1}, {2, 0}, {2, 2}};
+
+        int maxStones = removeMaxStonesFromQuarry.removeStones(stones);
+
+        Assertions.assertEquals(3, maxStones);
+    }
+
+    @Test
+    void testRemoveStones_3() {
+        int[][] stones = {{13,22},{2,29},{19,12},{17,27},{3,0},{3,11},{25,11},{3,21},{27,17},{18,19},{23,12},{5,1},{27,2},{24,0},{8,5},{13,21},{29,27},{16,17},{10,8},{19,18},{20,12},{18,27},{12,24},{2,23},{28,29},{17,24},{24,6},{12,10},{4,0},{6,15},{15,15},{12,3},{14,2},{2,6},{7,12},{26,26},{4,5},{20,26},{11,9},{15,10},{14,15},{18,28},{29,4},{6,12},{3,26},{0,8},{6,5},{15,24},{4,15},{15,21},{5,20},{20,28},{23,0},{5,27},{2,3},{8,27},{2,4}};
+
+        int maxStones = removeMaxStonesFromQuarry.removeStones(stones);
+
+        System.out.println(maxStones);
+    }
 }
